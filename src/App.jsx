6f6f954833131fdd73e743227e280e3f6c373631
Doruk Demirci <dorukdemirci06@gmail.com>
2026-09-17 16:09:15 +0300

    Fix shadow bounds on hero image to remove empty white space

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function App() {
             </a>
           </div>
           <div className="relative flex items-center justify-center">
-             <img src="/images/Art studio.jpg" alt="Art Studio" className="w-full h-auto max-h-[600px] object-contain rounded-3xl shadow-2xl shadow-stone-300" />
+             <img src="/images/Art studio.jpg" alt="Art Studio" className="h-auto max-h-[600px] max-w-full rounded-3xl shadow-2xl shadow-stone-400/50" />
           </div>
         </div>
       </section>
